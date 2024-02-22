00fddf96348d50ca0f5f37b70d1f657abf6567ae
fureunsan <fureunsan@naver.com>
2024-02-23 08:53:08 +0900

    2nd

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		log.debug("started main DemoApplication ");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
